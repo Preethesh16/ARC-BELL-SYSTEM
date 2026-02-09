d24b184f9dccd6cae60c1b9c3d29e3e4a7d001c1
Preethesh16 <preetheshcarvalho57@gmail.com>
2026-02-09 22:15:00 +0530

    firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,71 @@
 #include <Arduino.h>
 #include "secrets.h"
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <Firebase_ESP_Client.h>
 
-#define RELAY_PIN 0   // ESP-01 relay boards usually GPIO0
+#define RELAY_PIN 0
 
-ESP8266WebServer server(80);
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
-void handleRoot() {
-  String page = "<h2>ESP Bell Control</h2>";
-  page += "<a href='/on'><button style='font-size:30px'>ON</button></a><br><br>";
-  page += "<a href='/off'><button style='font-size:30px'>OFF</button></a>";
-  server.send(200, "text/html", page);
-}
-
-void handleOn() {
-  digitalWrite(RELAY_PIN, LOW);
-  server.send(200, "text/html", "Relay ON");
-}
-
-void handleOff() {
-  digitalWrite(RELAY_PIN, HIGH);
-  server.send(200, "text/html", "Relay OFF");
-}
+unsigned long lastCheck = 0;
 
 void setup() {
   Serial.begin(115200);
+  delay(2000);
 
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-
+  // -------- WiFi ----------
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("WiFi connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  Serial.println("\nWiFi OK");
 
-  Serial.println();
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  // -------- Firebase ----------
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
 
-  server.on("/", handleRoot);
-  server.on("/on", handleOn);
-  server.on("/off", handleOff);
+  // anonymous auth
+  auth.user.email = "";
+  auth.user.password = "";
 
-  server.begin();
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  Serial.println("Firebase ready");
 }
 
 void loop() {
-  server.handleClient();
+
+  if (millis() - lastCheck > 5000) {
+    lastCheck = millis();
+
+    // ---- read mode ----
+    if (Firebase.RTDB.getString(&fbdo, "/bellSystem/mode")) {
+      Serial.print("Mode = ");
+      Serial.println(fbdo.stringData());
+    } else {
+      Serial.print("Mode read error: ");
+      Serial.println(fbdo.errorReason());
+    }
+
+    // ---- manual ring ----
+    if (Firebase.RTDB.getBool(&fbdo, "/bellSystem/manualRing")) {
+      if (fbdo.boolData()) {
+        Serial.println("Manual ring trigger!");
+
+        digitalWrite(RELAY_PIN, LOW);
+        delay(3000);
+        digitalWrite(RELAY_PIN, HIGH);
+
+        Firebase.RTDB.setBool(&fbdo, "/bellSystem/manualRing", false);
+      }
+    }
+  }
 }
